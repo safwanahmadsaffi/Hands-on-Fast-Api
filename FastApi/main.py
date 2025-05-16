3e9b04c241ca2033937bf248bdcd85be670f0c4a
safwaahmad <sahmedsaffi@gmail.com>
2025-05-16 13:12:22 +0500

    Add patient management API with data loading functionality

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
-#decorator 
+def load_data(): #to load data from json file
+    #open the json file
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data
+#decorator -- endpoints
 @app.get("/") #to get request from api route to get fatch 
 def hello():
-    return {"message": "hello world"} #returning json data
+    return {"message": "Patient management system api"} #returning json data
+
 @app.get("/about")
 def about():
-    return {"message": "this is about page"}    
-    
+    return {"message": "fully functional api to manage patient records"}    
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
