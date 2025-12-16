de276a7e62cf384c3b522bb9533000bda8922030
safwanahmadsaffi <22ntucs1207@student.ntu.edu.pk>
2025-12-16 16:46:38 +0500

    Main Implement student management endpoints with filtering and retrieval functionality

---
+++ endpoints/get.py
@@ -0,0 +1,46 @@
+
+#---WE CAN MAKE MULTIPLE GET METHEOD AND USE BUT ENDPOINTS MUST BE UNIQUE---
+from fastapi import FastAPI, Path 
+
+app = FastAPI()
+
+students = [
+    "Safwan Ahmad Saffi","Ali", "Ahmed", "Hassan", "Hussain", "Omar",
+    "Zain", "Faizan", "Saad", "Shahzaib", "Hamza",
+    "Bilal", "Kashif", "Asad", "Salman", "Imran",
+    "Rizwan", "Usman", "Noman", "Junaid", "Rehan",
+    "Ayesha", "Fatima", "Zainab", "Maria", "Hira",
+    "Sana", "Iqra", "Maham", "Laiba", "Mariam",
+    "Anum", "Khadija", "Hafsa", "Areeba", "Mehwish",
+    "Zoya", "Amna", "Rabia", "Nida", "Aiman"
+]
+
+#by default endpoint is same as function name
+@app.get("/")
+def root():
+    return {"Message" : "hello world"}
+
+#filtering students whose names start with specific letters
+@app.get("/filter-students")
+def filter_students(sw: str,):
+    filtered_names = []
+    sw = sw.casefold()
+    for student in students:
+        if student.casefold().startswith(sw):
+            filtered_names.append(student)
+    return filtered_names
+
+#list of all students
+@app.get("/students")
+def get_students(): # type: ignore
+    return students
+
+#this is static endpoint
+@app.get("/students/Safwan Ahmad Saffi")
+def get_students():  # noqa: F811
+    return "Safwan Ahmad Saffi"
+
+#this is dynamic endpoint
+@app.get("/students/{id}")
+def get_student(id: int = Path(ge=0, lt=len(students))): #path manage krta hai validation ko id k lye
+    return students[id]
